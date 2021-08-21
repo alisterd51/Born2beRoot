60ef2f9a4975cdfd45c4c8cb959a9991b1377088
Antoine CLARMAN <anclarma@e1r12p7.42.fr>
2021-08-21 02:04:22 +0200

    manque fonction et manque affichage au propre

--- monitoring.sh
+++ monitoring.sh
@@ -5,15 +5,17 @@ function print_all()
 	/bin/wall $@
 }
 
-#print_all "bonjour" "tout le monde!"
-uname -a
-lscpu
-free -m
-df -h
-top -bn1
-who -b
+ARCH=$(uname -a)
+CPU=$(lscpu)
 
-netstat -a
-who --count
-hostname -I
-cat /var/log/sudo.log | wc -l
+MEM_USE=$(free -m)
+DISK_USE=$(df -h)
+CPU_USE=$(top -bn1)
+LAST_BOOT=$(who -b)
+
+TCP=$(netstat -a)
+USER_LOG=$(who --count)
+NET=$(hostname -I)
+SUDO=$(cat /var/log/sudo.log | wc -l)
+
+wall "$ARCH $CPU $MEM_USE $DISK_USE $CPU_USE $LAST_BOOT $TCP $USER_LOG $NET $SUDO"
